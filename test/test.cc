96933ff95cfdfcd4fe70b9dda6e62ffb0ce245ad
Taylor Woll <taylor.woll@gmail.com>
2018-12-10 00:55:43 -0800

    Improve test harness a bit

--- test/test.cc
+++ test/test.cc
@@ -12,32 +12,47 @@
 #include "NeuralNetwork.h"
 #include "TrainingData.h"
 
+void MakeXorTwoBitTrainingData(TrainingData* trainingData) {
+    trainingData->resize(4);
+    trainingData->at(0)._input = { 0.0, 0.0 };
+    trainingData->at(0)._output = { 0.0 };
+    trainingData->at(1)._input = { 1.0, 1.0 };
+    trainingData->at(1)._output = { 0.0 };
+    trainingData->at(2)._input = { 1.0, 0.0 };
+    trainingData->at(2)._output = { 1.0 };
+    trainingData->at(3)._input = { 0.0, 1.0 };
+    trainingData->at(3)._output = { 1.0 };
+}
+
+void MakeTestNetwork(NeuralNetwork* nn, TrainingData* trainingData) {
+    nn->SetInputNeuronCount(trainingData->at(0)._input.size());
+    nn->SetOutputNeuronCount(trainingData->at(0)._output.size());
+    nn->AddHiddenLayer(5);
+    nn->AddHiddenLayer(5);
+    nn->AddHiddenLayer(5);
+    nn->Construct();
+}
+
+void TrainAndTestNetwork(NeuralNetwork* nn, TrainingData* trainingData, NeuralNetwork::TrainingAlgorithmType algorithm, size_t epochs) {
+    nn->InitializeWeightsRandom();
+    nn->SetTrainingAlgorithmType(algorithm);
+    std::cout << "Before training error: " << nn->GetError(trainingData) << std::endl;
+    nn->Train(trainingData, epochs);
+    std::cout << "After training error: " << nn->GetError(trainingData) << std::endl;
+}
+
 int main(int argc, const char** argv) {
     TrainingData trainingData;
-    trainingData.resize(4);
-    trainingData[0]._input = { 0.0, 0.0 };
-    trainingData[0]._output = { 0.0 };
-    trainingData[1]._input = { 1.0, 1.0 };
-    trainingData[1]._output = { 0.0 };
-    trainingData[2]._input = { 1.0, 0.0 };
-    trainingData[2]._output = { 1.0 };
-    trainingData[3]._input = { 0.0, 1.0 };
-    trainingData[3]._output = { 1.0 };
+    MakeXorTwoBitTrainingData(&trainingData);
 
     NeuralNetwork nn;
-    nn.SetTrainingAlgorithmType(NeuralNetwork::TrainingAlgorithmType::SimulatedAnnealingResilientBackpropagation);
-    nn.SetInputNeuronCount(2);
-    nn.SetOutputNeuronCount(1);
-    nn.AddHiddenLayer(5);
-    nn.AddHiddenLayer(5);
-    nn.AddHiddenLayer(5);
-    nn.Construct();
-    nn.InitializeWeightsRandom();
+    MakeTestNetwork(&nn, &trainingData);
 
-    std::cout << "Before training error: " << nn.GetError(&trainingData) << std::endl;
-    nn.Train(&trainingData, 10000);
-
-    std::cout << "After training error: " << nn.GetError(&trainingData) << std::endl;
+    TrainAndTestNetwork(&nn, &trainingData, NeuralNetwork::TrainingAlgorithmType::Backpropagation, 1000);
+    TrainAndTestNetwork(&nn, &trainingData, NeuralNetwork::TrainingAlgorithmType::BatchingBackpropagation, 1000);
+    TrainAndTestNetwork(&nn, &trainingData, NeuralNetwork::TrainingAlgorithmType::QuickBackpropagation, 1000);
+    TrainAndTestNetwork(&nn, &trainingData, NeuralNetwork::TrainingAlgorithmType::ResilientBackpropagation, 1000);
+    TrainAndTestNetwork(&nn, &trainingData, NeuralNetwork::TrainingAlgorithmType::SimulatedAnnealingResilientBackpropagation, 1000);
 
     return 0;
 }
